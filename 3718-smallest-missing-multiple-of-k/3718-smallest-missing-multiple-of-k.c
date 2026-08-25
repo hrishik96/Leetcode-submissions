c62bf3106e67678799300e5004d706413ba9cee5
Hrishik N Urs <hrishikhushi96@gmail.com>
2026-08-25 09:41:13 +0530

    Time: 0 ms (100%), Space: 9.7 MB (60.53%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.c
@@ -0,0 +1,25 @@
+int missingMultiple(int* nums, int numsSize, int k) {
+    int *arr;
+    arr=(int*)calloc(numsSize+2,sizeof(int));
+
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]%k==0){
+            long int ind=nums[i]/k;
+            if(ind>=0 && ind<=numsSize){
+                arr[ind]++;
+            }
+        }
+
+
+    }
+    int res;
+    for(int i=1;i<=numsSize+1;i++){
+        if(arr[i]==0){
+            res = i*k;
+            break;
+        }
+    }
+
+    free(arr);
+    return res;
+}
