589ae27e280cf713ba5a9e838730e40da85a623f
Hrishik N Urs <hrishikhushi96@gmail.com>
2026-08-27 18:58:56 +0530

    Time: 0 ms (100%), Space: 10.4 MB (94.19%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.c
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.c
@@ -1,46 +1,39 @@
 int* searchRange(int* nums, int numsSize, int target, int* returnSize) {
-    int* arr = (int*)malloc(2 * sizeof(int));
-    *returnSize = 2;
-    arr[0] = -1;
-    arr[1] = -1;
-
-    if (numsSize == 0) {
+    int* arr=(int*)malloc(2*sizeof(int));
+    *returnSize=2;
+    arr[0]=-1;
+    arr[1]=-1;
+    if(numsSize==0){
         return arr;
     }
-
-    int low = 0;
-    int high = numsSize - 1;
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
-        if (nums[mid] == target) {
-            arr[0] = mid;     
-            high = mid - 1;   
-        } else if (nums[mid] > target) {
-            high = mid - 1;
-        } else {
-            low = mid + 1;
+    int low=0;
+    int high=numsSize-1;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(nums[mid]==target){
+            arr[0]=mid;
+            high=mid-1;
+        }else if(nums[mid]>target){
+            high=mid-1;
+        }else{
+            low=mid+1;
         }
     }
-
-    
-    if (arr[0] == -1) {
+    if(arr[0]==-1){
         return arr;
     }
-
-    
-    low = 0;
-    high = numsSize - 1;
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
-        if (nums[mid] == target) {
-            arr[1] = mid;     
-            low = mid + 1;    
-        } else if (nums[mid] > target) {
-            high = mid - 1;
-        } else {
-            low = mid + 1;
+    low=0;
+    high=numsSize-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target){
+            arr[1]=mid;
+            low=mid+1;
+        }else if(nums[mid]>target){
+            high=mid-1;
+        }else{
+            low=mid+1;
         }
     }
-
     return arr;
 }
