6562e9e432bf66505a5a6bfe184e226bf4c5080a
Hrishik N Urs <hrishikhushi96@gmail.com>
2026-09-03 19:03:45 +0530

    Time: 2 ms (67.61%), Space: 8.5 MB (40.16%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.c
@@ -0,0 +1,18 @@
+
+
+int countOdds(int low, int high){
+    if((high-low+1)%2==0){
+        return (high-low+1)/2;
+
+    }
+    else{
+        if(high%2==0 && low%2==0){
+            return (high-low+1)/2;
+        }
+        else{
+            return (((high-low+1)/2)+1);
+
+        }
+    }
+
+}
